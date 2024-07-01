953038129ba5fcd538ddf9245418d3356ec72a5d
semnil5202 <semnil5202@gmail.com>
2024-07-02 04:35:02 +0900

    chore: eslint import order과 vscode setting json import 룰이 충돌나는 오류 수정

--- packages/core/config/eslint/next.js
+++ packages/core/config/eslint/next.js
@@ -33,7 +33,7 @@ module.exports = {
     'import/order': [
       'warn',
       {
-        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
         alphabetize: {
           order: 'asc',
           caseInsensitive: true,
