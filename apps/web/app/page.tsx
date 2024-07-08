9f7cca93e080c050ea9cfc16c3a2d2182c5372e8
Jong <abg3000@naver.com>
2024-07-08 22:05:31 +0900

    chore: 배포 테스트 (#50)
    * chore:test
    
    * chore:test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
 import { FirstDomainExampleScreen } from '@sambad/web-domains/first-domain';
+import { useEffect } from 'react';
 
 import styles from './page.module.css';
 
 export default function Home() {
-  console.log('test');
+  useEffect(() => {
+    console.log('test-main');
+  }, []);
 
   return (
     <main className={styles.main}>
