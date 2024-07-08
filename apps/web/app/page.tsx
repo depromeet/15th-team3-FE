6c87a231f9043bcf01606568a4ff8229b1b958a9
abg3000 <abg3000@naver.com>
2024-07-08 21:53:43 +0900

    chore: test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,8 @@ import { FirstDomainExampleScreen } from '@sambad/web-domains/first-domain';
 import styles from './page.module.css';
 
 export default function Home() {
+  console.log('test');
+
   return (
     <main className={styles.main}>
       <FirstDomainExampleScreen />
