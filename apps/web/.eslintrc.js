6eefb29187df6e9dd56e8e7763dc99d3909d80bf
semnil5202 <semnil5202@gmail.com>
2024-07-14 02:00:09 +0900

    chore: apps/web storybook init

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@sambad/eslint-config/next.js'],
+  extends: ['@sambad/eslint-config/next.js', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
