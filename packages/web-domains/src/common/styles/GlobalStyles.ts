9ff797f8fc6890959a21cf320eabc284283056f7
semnil5202 <semnil5202@gmail.com>
2024-07-18 05:06:10 +0900

    style: GlobalStyles indent 수정

--- packages/web-domains/src/common/styles/GlobalStyles.ts
+++ packages/web-domains/src/common/styles/GlobalStyles.ts
@@ -1,97 +1,97 @@
 import { injectGlobal } from '@emotion/css';
 
 export const injectGlobalStyles = () => injectGlobal`
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-}
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
 
-html {
-  -moz-text-size-adjust: none;
-  -webkit-text-size-adjust: none;
-  text-size-adjust: none;
-}
+  html {
+    -moz-text-size-adjust: none;
+    -webkit-text-size-adjust: none;
+    text-size-adjust: none;
+  }
 
-body,
-h1,
-h2,
-h3,
-h4,
-p,
-figure,
-blockquote,
-dl,
-dd {
-  margin-block-end: 0;
-}
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    margin-block-end: 0;
+  }
 
-ul[role='list'],
-ol[role='list'] {
-  list-style: none;
-}
+  ul[role='list'],
+  ol[role='list'] {
+    list-style: none;
+  }
 
-body {
-  max-width: 100vw;
-  min-height: 100vh;
-  line-height: 1.5;
-  overflow-x: hidden;
-}
+  body {
+    max-width: 100vw;
+    min-height: 100vh;
+    line-height: 1.5;
+    overflow-x: hidden;
+  }
 
-h1,
-h2,
-h3,
-h4,
-button,
-input,
-label {
-  line-height: 1.1;
-}
+  h1,
+  h2,
+  h3,
+  h4,
+  button,
+  input,
+  label {
+    line-height: 1.1;
+  }
 
-a:not([class]) {
-  text-decoration-skip-ink: auto;
-  color: currentColor;
-}
+  a:not([class]) {
+    text-decoration-skip-ink: auto;
+    color: currentColor;
+  }
 
-img,
-picture {
-  max-width: 100%;
-  display: block;
-}
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
 
-input,
-button,
-textarea,
-select {
-  font: inherit;
-}
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
 
-input {
-  border: none;
-}
+  input {
+    border: none;
+  }
 
-textarea {
-  resize: none;
-  border: none;
-}
+  textarea {
+    resize: none;
+    border: none;
+  }
 
-button {
-  border: none;
-  background: none;
-}
+  button {
+    border: none;
+    background: none;
+  }
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 
-textarea:not([rows]) {
-  min-height: 10em;
-}
+  textarea:not([rows]) {
+    min-height: 10em;
+  }
 
-:target {
-  scroll-margin-block: 5ex;
-}
+  :target {
+    scroll-margin-block: 5ex;
+  }
 `;
