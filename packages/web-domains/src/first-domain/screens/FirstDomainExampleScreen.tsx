673f29513639d714050f26e296720b767498422c
abg3000 <abg3000@naver.com>
2024-07-18 02:25:01 +0900

    feat: ProgressIndicator 컴포넌트

--- packages/web-domains/src/first-domain/screens/FirstDomainExampleScreen.tsx
+++ packages/web-domains/src/first-domain/screens/FirstDomainExampleScreen.tsx
@@ -8,7 +8,6 @@ export const FirstDomainExampleScreen = async () => {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <div>
-        <h1>도메인 화면을 전체 담당 하는 컴포넌트입니다.</h1>
         <FirstFeatureOfFirstDomainTestContainer />
       </div>
     </HydrationBoundary>
