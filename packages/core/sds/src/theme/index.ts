2cdb15b1ef88336105908e1dcc3864dbc57f60bd
Doeunnkimm <doeunnkimm@gmail.com>
2024-07-23 22:16:31 +0900

    fix: borderRadius → borderRadiusVariants

--- packages/core/sds/src/theme/index.ts
+++ packages/core/sds/src/theme/index.ts
@@ -1,4 +1,4 @@
-export { borderRadius } from './borderRadius';
+export { borderRadiusVariants } from './borderRadius';
 export { colors } from './colors';
 export { opacity } from './opacity';
 export { shadow } from './shadow';
