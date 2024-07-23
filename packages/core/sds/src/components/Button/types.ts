2a377480917b3facf32087364f7f307cfc7f940e
Doeunnkimm <doeunnkimm@gmail.com>
2024-07-23 22:15:55 +0900

    fix: 버튼 컴포넌트 수정

--- packages/core/sds/src/components/Button/types.ts
+++ packages/core/sds/src/components/Button/types.ts
@@ -1,12 +1,2 @@
-import { borderRadius } from '../../theme/borderRadius';
-
-export type Size = 'large' | 'medium';
-export type Variant = 'primary' | 'sub' | 'text';
-export type Radius = keyof typeof borderRadius;
-
-export interface ButtonCssArg {
-  size: Size;
-  variant: Variant;
-  radius: Radius;
-  isDisabled?: boolean;
-}
+export type ButtonSize = 'medium' | 'large';
+export type ButtonVariant = 'primary' | 'sub';
