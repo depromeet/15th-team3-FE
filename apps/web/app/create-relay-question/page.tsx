15210de7777c72978b9abf84912098c6c6ee575a
semnil5202 <semnil5202@gmail.com>
2024-07-25 02:28:48 +0900

    feat: CreateRelayQuestionScreen page.tsx로 연동

---
+++ apps/web/app/create-relay-question/page.tsx
@@ -0,0 +1,9 @@
+import { CreateRelayQuestionScreen } from '@sambad/web-domains/create-relay-question';
+
+export default function CreateRelayQuestion() {
+  return (
+    <main>
+      <CreateRelayQuestionScreen />
+    </main>
+  );
+}
