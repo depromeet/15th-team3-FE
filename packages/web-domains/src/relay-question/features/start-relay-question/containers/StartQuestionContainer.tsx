a51853f4e47358cb6cc6389ddaf4d004b224b7cd
semnil5202 <semnil5202@gmail.com>
2024-07-25 23:28:58 +0900

    feat: select relay page로 라우팅 기능 추가

--- packages/web-domains/src/relay-question/features/start-relay-question/containers/StartQuestionContainer.tsx
+++ packages/web-domains/src/relay-question/features/start-relay-question/containers/StartQuestionContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@sambad/sds/components';
+import { useRouter } from 'next/navigation';
 
 import { RelayStartBackground } from '../../../assets/RealyStartBackground';
 import { Z_INDEX } from '../../../constants';
@@ -9,12 +10,16 @@ import { Profile } from '../components/Profile/Profile';
 import { StartQuestionBackgroundCss, StartQuestionContainerCss } from './StartQuestionContainer.styles';
 
 export const StartQuestionContainer = () => {
+  const router = useRouter();
+
   return (
     <div css={StartQuestionContainerCss}>
       <Profile />
 
       <section css={StartQuestionBackgroundCss}>
-        <Button style={{ zIndex: Z_INDEX.relayStartButton }}>시작하기</Button>
+        <Button style={{ zIndex: Z_INDEX.relayStartButton }} onClick={() => router.push('/select-relay-question')}>
+          시작하기
+        </Button>
         <RelayStartBackground css={{ width: '100%', position: 'absolute', bottom: 0 }} />
       </section>
     </div>
