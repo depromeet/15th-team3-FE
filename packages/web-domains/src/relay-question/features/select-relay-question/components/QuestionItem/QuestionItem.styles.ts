8a92015acd297c9feb054850633da80ad190493b
semnil5202 <semnil5202@gmail.com>
2024-07-26 00:37:39 +0900

    feat: 릴레이 질문 리스트 나열 기능 추가

---
+++ packages/web-domains/src/relay-question/features/select-relay-question/components/QuestionItem/QuestionItem.styles.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+import { borderRadiusVariants, size } from '@sambad/sds/theme';
+
+export const questionItemCss = css({
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: `${size['3xs']}`,
+  padding: `${size['5xs']} ${size['2xs']}`,
+});
+
+export const questionTextBoxCss = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: `${size['6xs']}`,
+  justifyContent: 'center',
+});
+
+export const questionImgBoxCss = css({
+  minWidth: `${size['4xl']}`,
+  minHeight: `${size['4xl']}`,
+  borderRadius: `${borderRadiusVariants.medium}`,
+});
