f6771dcd4db866862949c91d41a9657fb1420a5b
abg3000 <abg3000@naver.com>
2024-07-27 00:42:20 +0900

    chore: delete unused domains

--- apps/web/app/second/page.tsx
+++ apps/web/app/second/page.tsx
@@ -1,11 +1,5 @@
-import { SecondDomainExampleScreen } from '@sambad/web-domains/second-domain';
-
 import styles from '../page.module.css';
 
 export default function Second() {
-  return (
-    <main className={styles.main}>
-      <SecondDomainExampleScreen />
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
