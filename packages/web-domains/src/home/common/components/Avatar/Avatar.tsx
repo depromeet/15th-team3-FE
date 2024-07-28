5eec2612e80b1f1dc592f2b7bcfd809a23da3061
abg3000 <abg3000@naver.com>
2024-07-28 17:46:56 +0900

    refactor: home 공통 컴포넌트 components 밑으로 이동

---
+++ packages/web-domains/src/home/common/components/Avatar/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image, { ImageProps } from 'next/image';
+
+interface AvatarProps extends Omit<ImageProps, 'src' | 'alt'> {
+  imageUrl?: string;
+  alt?: string;
+  size?: number;
+  Icon?: React.ComponentType<{ size?: number }>;
+}
+
+export const Avatar = ({ imageUrl, alt = 'profile-image', size, Icon, ...rest }: AvatarProps) => {
+  if (!imageUrl) {
+    return (
+      <span css={{ display: 'inline-flex' }} {...rest}>
+        {Icon && <Icon size={size} />}
+      </span>
+    );
+  }
+
+  return <Image src={imageUrl} alt={alt} fill {...rest} />;
+};
---
+++ packages/web-domains/src/home/common/components/Layout/PreviousQuestionLayout.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from 'react';
+
+export const PreviousQuestionLayout = ({ children }: PropsWithChildren) => {
+  return <div style={{ height: '100%' }}>{children}</div>;
+};
