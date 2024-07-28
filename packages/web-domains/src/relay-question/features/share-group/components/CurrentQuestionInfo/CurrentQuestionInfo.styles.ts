6e6e891247e3d8742feb7ffa73e2d1fe4d91037b
semnil5202 <semnil5202@gmail.com>
2024-07-29 04:49:49 +0900

    feat: share-group background 및 currentQuestion Count 구현

---
+++ packages/web-domains/src/relay-question/features/share-group/components/CurrentQuestionInfo/CurrentQuestionInfo.styles.ts
@@ -0,0 +1,35 @@
+import { css } from '@emotion/react';
+import { borderRadiusVariants, colors, size } from '@sambad/sds/theme';
+
+export const wrapperCss = css({
+  width: '140px',
+  position: 'absolute',
+  top: 72,
+  left: '50%',
+  transform: 'translateX(-50%)',
+});
+
+export const countWrapperCss = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: `${size['6xs']} ${size['2xs']}`,
+  borderRadius: `${borderRadiusVariants.large}`,
+  backgroundColor: `${colors.primary100}`,
+  marginBottom: size['3xl'],
+});
+
+export const profileWrapperCss = css({
+  display: 'flex',
+  justifyContent: 'end',
+  alignItems: 'center',
+  gap: size['7xs'],
+});
+
+export const imgWrapperCss = css({
+  width: size['2xs'],
+  height: size['2xs'],
+  borderRadius: borderRadiusVariants.round,
+});
+
+export const SVGDecoPositionCss = css;
