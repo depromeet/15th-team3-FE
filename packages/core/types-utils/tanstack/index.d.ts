e6b86a9e71f2a7f1079fc62e277de322f98b2267
Doeunnkimm <doeunnkimm@gmail.com>
2024-07-28 16:27:28 +0900

    refactor

--- packages/core/types-utils/tanstack/index.d.ts
+++ packages/core/types-utils/tanstack/index.d.ts
@@ -1,3 +1,4 @@
 import { UseQueryOptions } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
-export type UseQueryOptionsExcludedQueryKey<T, K = T> = Omit<UseQueryOptions<T, unknown, K>, 'queryKey'>;
+export type UseQueryOptionsExcludedQueryKey<T> = Omit<UseQueryOptions<AxiosResponse<T>, unknown, T>, 'queryKey'>;
