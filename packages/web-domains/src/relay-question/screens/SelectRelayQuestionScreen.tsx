649e00d2b931bcd0acd48f416104db735ed9d6b4
semnil5202 <semnil5202@gmail.com>
2024-07-28 14:38:21 +0900

    feat: query string context 생성

--- packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
+++ packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
@@ -1,9 +1,14 @@
+import { ProgressIndicatorContainer } from '../features/select-relay-question/containers/ProgressIndicatorContainer/ProgressIndicatorContainer';
 import { QuestionList } from '../features/select-relay-question/containers/QuestionList/QuestionList';
+import { RandomPick } from '../features/select-relay-question/containers/RandomPick/RandomPick';
+import { QueryStringProvider } from '../features/select-relay-question/contexts/QueryStringContext';
 
 export const SelectRelayQuestionScreen = () => {
   return (
-    <>
+    <QueryStringProvider>
+      <ProgressIndicatorContainer />
       <QuestionList />
-    </>
+      <RandomPick />
+    </QueryStringProvider>
   );
 };
