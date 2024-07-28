1b67e166849cac093a22b2ff1a0868c011a65796
Doeunnkimm <doeunnkimm@gmail.com>
2024-07-28 23:21:25 +0900

    feat: Badge 컴포넌트

---
+++ packages/core/sds/src/components/Badge/Badge.tsx
@@ -0,0 +1,20 @@
+import { forwardRef, HTMLAttributes } from 'react';
+import { badgeCss } from './styles';
+import { colors } from '@/theme';
+
+export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
+  color?: string;
+}
+
+export const Badge = forwardRef<HTMLSpanElement, BadgeProps>((props, ref) => {
+  const { color = colors.primary500, style: styleFromProps, ...restProps } = props;
+
+  const style = {
+    backgroundColor: color,
+    ...styleFromProps,
+  };
+
+  return <span ref={ref} style={style} css={badgeCss} {...restProps} />;
+});
+
+Badge.displayName = 'Badge';
---
+++ packages/core/sds/src/components/Badge/styles.ts
@@ -0,0 +1,14 @@
+import { borderRadiusVariants, size } from '@/theme';
+import { css } from '@emotion/react';
+import { fontWeightVariants } from '../Typography/styles';
+
+export const badgeCss = css({
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: borderRadiusVariants.round,
+  padding: `${size['7xs']} ${size['5xs']}`,
+  fontSize: '14px',
+  fontWeight: fontWeightVariants.medium,
+  lineHeight: '150%',
+});
