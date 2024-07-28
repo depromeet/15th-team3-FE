a76aeb5d7fa1b40df4588d73c8ca12f1179f9358
semnil5202 <semnil5202@gmail.com>
2024-07-29 06:35:01 +0900

    feat: 다음 릴레이 질문자 선택 페이지 구현

---
+++ packages/web-domains/src/relay-question/features/share-next-questioner/containers/ShareNextQuestionerContainer/ShareNextQuestionerContainer.styles.ts
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import { colors, size } from '@sambad/sds/theme';
+
+export const wrapperCss = css({
+  padding: `${size.xl} ${size['2xs']}`,
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  minHeight: '100vh',
+  backgroundColor: colors.primary50,
+});
+
+export const headingWrapperCss = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingTop: size['2xl'],
+});
+
+export const footerWrapperCss = css({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export const buttonWrapperCss = css({
+  width: '100%',
+  maxWidth: '430px',
+});
+
+export const shareButtonCss = css({
+  margin: `${size.xl} 0 ${size['5xs']}`,
+});
--- packages/web-domains/src/relay-question/index.ts
+++ packages/web-domains/src/relay-question/index.ts
@@ -1,3 +1,4 @@
 export { SelectRelayQuestionScreen } from './screens/SelectRelayQuestionScreen';
 export { ShareGroupScreen } from './screens/ShareGroupScreen';
+export { ShareNextQuestionerScreen } from './screens/ShareNextQuestionerScreen';
 export { StartRelayQuestionScreen } from './screens/StartRelayQuestionScreen';
