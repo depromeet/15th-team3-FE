b10e0a69685e7641cf6939e87a3cb9002d6118dd
semnil5202 <semnil5202@gmail.com>
2024-07-28 14:39:13 +0900

    refactor: Question List 하단 여백 보장

--- packages/web-domains/src/relay-question/features/select-relay-question/containers/QuestionList/QuestionList.styles.ts
+++ packages/web-domains/src/relay-question/features/select-relay-question/containers/QuestionList/QuestionList.styles.ts
@@ -4,3 +4,7 @@ import { size } from '@sambad/sds/theme';
 export const questionTextBoxCss = css({
   padding: `${size.xl} ${size['2xs']} ${size.xs}`,
 });
+
+export const questionListCss = css({
+  paddingBottom: `${size['7xl']}`,
+});
--- packages/web-domains/src/relay-question/features/select-relay-question/containers/QuestionList/QuestionList.tsx
+++ packages/web-domains/src/relay-question/features/select-relay-question/containers/QuestionList/QuestionList.tsx
@@ -5,17 +5,17 @@ import { Txt } from '@sambad/sds/components';
 import PNGQuestionImage1 from '../../../../assets/png/question-image-1.png';
 import { QuestionItem } from '../../components/QuestionItem/QuestionItem';
 
-import { questionTextBoxCss } from './QuestionList.styles';
+import { questionListCss, questionTextBoxCss } from './QuestionList.styles';
 
 export const QuestionList = () => {
   return (
-    <>
+    <section>
       <div css={questionTextBoxCss}>
         <Txt typography="heading1" fontWeight="bold">
           어떤 질문으로 물어볼까요?
         </Txt>
       </div>
-      <section>
+      <ul css={questionListCss}>
         {new Array(10)
           .fill({
             imageUrl: PNGQuestionImage1,
@@ -25,7 +25,7 @@ export const QuestionList = () => {
           .map(({ imageUrl, title, usedCount }, index) => (
             <QuestionItem key={index} imageUrl={imageUrl} title={title} usedCount={usedCount} />
           ))}
-      </section>
-    </>
+      </ul>
+    </section>
   );
 };
