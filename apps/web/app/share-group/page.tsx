4aeffcdd7c17bd36f3a40fbbed06918f84ad4d7c
semnil5202 <semnil5202@gmail.com>
2024-07-29 05:16:57 +0900

    feat: 질문 생성 완료 및 그룹에게 공유 페이지 구현

--- apps/web/app/share-group/page.tsx
+++ apps/web/app/share-group/page.tsx
@@ -1,3 +1,9 @@
+import { ShareGroupScreen } from '@sambad/web-domains/relay-question';
+
 export default function ShareGroup() {
-  return <main>share group</main>;
+  return (
+    <main>
+      <ShareGroupScreen />
+    </main>
+  );
 }
