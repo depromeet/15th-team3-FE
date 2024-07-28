509916cb41b9b9af7d892c8c5021a85397d04a32
semnil5202 <semnil5202@gmail.com>
2024-07-28 16:11:47 +0900

    feat: Questioner 생성 및 모달 오픈 로직 hook 분리

---
+++ packages/web-domains/src/relay-question/features/select-relay-question/components/Questioner/Question.styles.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+import { borderRadiusVariants, size } from '@sambad/sds/theme';
+
+export const wrapperCss = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: `${size['5xs']}`,
+  padding: `${size['5xs']} ${size['2xs']}`,
+  cursor: 'pointer',
+});
+
+export const imgWrapperCss = css({
+  height: `${size.xl}`,
+  width: `${size.xl}`,
+  borderRadius: `${borderRadiusVariants.round}`,
+});
