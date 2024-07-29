9692db23605eb7c670bd5975dd113cccb4756bbb
Doeunnkimm <doeunnkimm@gmail.com>
2024-07-30 01:43:02 +0900

    feat: ProfileContainer

---
+++ packages/web-domains/src/about-me/features/components/Profile/Profile.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { borderRadiusVariants, colors, size } from '@sambad/sds/theme';
+import { badgeContainerCss, nameCss, rootCss } from './styles';
+import Image from 'next/image';
+import { Badge, Txt } from '@sambad/sds/components';
+
+interface ProfileProps {
+  imageUrl: string;
+  name: string;
+  gender?: 'MALE' | 'FEMALE';
+  birth?: string;
+  job?: string;
+  location?: string;
+  mbti?: string;
+}
+
+const colorMap = [colors.secondary100, colors.primary100, colors.tertiary50, colors.quaternary100, colors.primary200];
+
+export const Profile = (props: ProfileProps) => {
+  const { name, imageUrl, birth: birthFromProps, gender: genderFromProps, mbti, location, job } = props;
+
+  // FIXME: dayjs로 나이 변환 예정
+  const age = birthFromProps;
+  const gender = genderFromProps === 'FEMALE' ? '여자' : '남자';
+
+  const infoBadges = [age, gender, mbti, location, job].filter(Boolean);
+
+  return (
+    <section css={rootCss}>
+      <Image
+        src={imageUrl}
+        width={80}
+        height={80}
+        alt={`${name}_프로필_이미지`}
+        style={{ borderRadius: borderRadiusVariants.round }}
+      />
+      <Txt typography="heading2" css={nameCss}>
+        {name}
+      </Txt>
+      <div css={badgeContainerCss}>
+        {infoBadges.map((info, index) => (
+          <Badge key={info} color={colorMap[index]}>
+            #{info}
+          </Badge>
+        ))}
+      </div>
+    </section>
+  );
+};
---
+++ packages/web-domains/src/about-me/features/containers/ProfileContainer.tsx
@@ -0,0 +1,5 @@
+import { Profile } from '../components';
+
+export const ProfileContainer = () => {
+  return <Profile name="김시은" imageUrl="" birth="26세" gender="FEMALE" mbti="ENFJ" location="수원" job="취준생" />;
+};
