af07cbb78032f4fb6af71cc2288de5147217754d
semnil5202 <semnil5202@gmail.com>
2024-07-29 14:32:24 +0900

    fix: suspense err fix - 3

--- packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
+++ packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
@@ -1,19 +1,9 @@
-import { Suspense } from 'react';
-
 import { ContentContainer } from '../features/select-relay-question/containers/ContentContainer/ContentContainer';
 import { ProgressIndicatorContainer } from '../features/select-relay-question/containers/ProgressIndicatorContainer/ProgressIndicatorContainer';
 import { RandomPickContainer } from '../features/select-relay-question/containers/RandomPickContainer/RandomPickContainer';
 import { QueryStringProvider } from '../features/select-relay-question/contexts/QueryStringContext';
 
 export const SelectRelayQuestionScreen = async () => {
-  return (
-    <Suspense>
-      <SelectRelayQuestion />
-    </Suspense>
-  );
-};
-
-const SelectRelayQuestion = () => {
   return (
     <QueryStringProvider>
       <ProgressIndicatorContainer />
--- packages/web-domains/src/relay-question/screens/StartRelayQuestionScreen.tsx
+++ packages/web-domains/src/relay-question/screens/StartRelayQuestionScreen.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
+
 import { StartQuestionContainer } from '../features/start-relay-question/containers/StartQuestionContainer/StartQuestionContainer';
 
 export const StartRelayQuestionScreen = async () => {
   return (
-    <>
+    <Suspense>
       <StartQuestionContainer />
-    </>
+    </Suspense>
   );
 };
