c4122fe91f75974614dd2d665d8e1cd03aebba6f
leejeongho <jh9628771@gmail.com>
2024-07-30 14:44:49 +0900

    feat: TextInput컴포넌트 생성

---
+++ packages/web-domains/src/user/features/get-user-info/components/TextInput/styles.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+import { colors, borderRadiusVariants, size } from '@sambad/sds/theme';
+
+export const inputCss = css({
+  display: 'block',
+  width: '100%',
+  backgroundColor: colors.grey200,
+  border: `1px solid ${colors.grey400}`,
+  padding: '12px 16px',
+  borderRadius: borderRadiusVariants.medium,
+  ':focus': { outline: `1px solid ${colors.grey600}` },
+});
+
+export const textInputcss = css({
+  display: 'flex',
+  flexDirection: 'column',
+  '& > *:not(:first-child)': { marginTop: size['6xs'] },
+});
