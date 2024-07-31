e84d91f01ba28a70c99d3291b2126b5537002cbf
semnil5202 <semnil5202@gmail.com>
2024-07-31 23:06:47 +0900

    refactor: useModal refactoring using toss-overlay

---
+++ packages/web-domains/src/relay-question/features/select-relay-question/hooks/useModal.tsx
@@ -0,0 +1,49 @@
+import { useOverlay } from '@toss/use-overlay';
+import { ComponentType, useCallback } from 'react';
+
+import { Modal } from '../../../common/Modal';
+
+interface ModalContentProps {
+  onConfirm: () => void;
+  onClose: () => void;
+}
+
+interface OpenModalProps<T> {
+  component: ComponentType<T & ModalContentProps>;
+  componentProps: T;
+}
+
+export const useModal = () => {
+  const overlay = useOverlay();
+
+  const openModal = useCallback(
+    <T,>({ component: Component, componentProps }: OpenModalProps<T>) => {
+      return new Promise<boolean>((resolve) => {
+        overlay.open(({ isOpen, close }) => (
+          <Modal
+            isOpen={isOpen}
+            onClose={() => {
+              resolve(false);
+              close();
+            }}
+          >
+            <Component
+              {...componentProps}
+              onConfirm={() => {
+                resolve(true);
+                close();
+              }}
+              onClose={() => {
+                resolve(false);
+                close();
+              }}
+            />
+          </Modal>
+        ));
+      });
+    },
+    [overlay],
+  );
+
+  return openModal;
+};
