0ce5f36aa3957981fecd421e4c39b5188f84290f
semnil5202 <semnil5202@gmail.com>
2024-07-31 23:09:47 +0900

    refactor: prefetch select relay question screen

--- packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
+++ packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
@@ -1,18 +1,40 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { Suspense } from 'react';
 
 import { ContentContainer } from '../features/select-relay-question/containers/ContentContainer/ContentContainer';
 import { ProgressIndicatorContainer } from '../features/select-relay-question/containers/ProgressIndicatorContainer/ProgressIndicatorContainer';
 import { RandomPickContainer } from '../features/select-relay-question/containers/RandomPickContainer/RandomPickContainer';
 import { QueryStringProvider } from '../features/select-relay-question/contexts/QueryStringContext';
+import { useRelayQuestionListQueryPrefetch } from '../features/select-relay-question/hooks/queries/useRelayQuestionListQuery';
+
+const MEETING_ID = 1;
 
 export const SelectRelayQuestionScreen = async () => {
+  const queryClient = await getServerSideProps();
+
   return (
-    <Suspense>
-      <QueryStringProvider>
-        <ProgressIndicatorContainer />
-        <ContentContainer />
-        <RandomPickContainer />
-      </QueryStringProvider>
-    </Suspense>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense>
+        <QueryStringProvider>
+          <ProgressIndicatorContainer />
+          <ContentContainer />
+          <RandomPickContainer />
+        </QueryStringProvider>
+      </Suspense>
+    </HydrationBoundary>
   );
+};
+
+const getServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  try {
+    const gatherMemberPrefetch = useRelayQuestionListQueryPrefetch({ queryClient, meetingId: MEETING_ID });
+
+    await Promise.all([gatherMemberPrefetch]);
+  } catch (error: unknown) {
+    console.log(error);
+  }
+
+  return queryClient;
 };
