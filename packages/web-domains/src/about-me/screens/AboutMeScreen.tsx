18a5af6c9c28a7ce37e8fd67e6b3563dfe2e5261
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-01 23:46:26 +0900

    feat: screen

--- packages/web-domains/src/about-me/screens/AboutMeScreen.tsx
+++ packages/web-domains/src/about-me/screens/AboutMeScreen.tsx
@@ -1,3 +1,23 @@
+'use client';
+
+import { colors, size } from '@sambad/sds/theme';
+
+import { ProfileContainer } from '../features/containers/ProfileContainer';
+import { SegmentedControlContainer } from '../features/containers/SegmentedControlContainer';
+
 export const AboutMeScreen = () => {
-  return <div></div>;
+  return (
+    <div style={layoutStyle}>
+      <ProfileContainer style={{ marginBottom: size['5xs'] }} />
+      <SegmentedControlContainer style={sectionStyle} />
+    </div>
+  );
+};
+
+const layoutStyle = {
+  backgroundColor: colors.grey200,
+};
+
+const sectionStyle = {
+  marginTop: size['5xs'],
 };
