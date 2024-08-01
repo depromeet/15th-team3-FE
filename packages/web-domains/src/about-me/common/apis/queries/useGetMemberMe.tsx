b4bfe63aaa82c2709c6c27314163bf34215f21f9
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-01 23:58:46 +0900

    feat: query, schema

---
+++ packages/web-domains/src/about-me/common/apis/queries/useGetMemberMe.tsx
@@ -0,0 +1,42 @@
+import { QueryClient, useQuery, UseQueryOptions } from '@tanstack/react-query';
+
+import { Http } from '@/common/apis/base.api';
+
+import { MeetingMemberResponse } from '../schema/MeetingMemberResponse';
+
+interface Params {
+  meetingId: number;
+}
+
+interface QueryProps extends Params {
+  options?: UseQueryOptions<MeetingMemberResponse>;
+}
+
+export const MEMBER_ME_QUERY_KEY = 'MEMBER_ME_QUERY_KEY';
+
+const queryFn = ({ meetingId }: Params) => Http.GET<MeetingMemberResponse>(`/v1/meetings/${meetingId}/members/me`);
+
+export const useGetMemberMe = (props: QueryProps) => {
+  const { options, ...params } = props;
+
+  return useQuery({
+    queryKey: [MEMBER_ME_QUERY_KEY, params],
+    queryFn: () => queryFn(params),
+    ...options,
+  });
+};
+
+interface PrefetchProps extends Params {
+  queryClient: QueryClient;
+}
+
+export const getMemberMePrefetch = (props: PrefetchProps) => {
+  const { queryClient, ...params } = props;
+
+  const prefetch = queryClient.prefetchQuery({
+    queryKey: [MEMBER_ME_QUERY_KEY, params],
+    queryFn: () => queryFn(params),
+  });
+
+  return prefetch;
+};
