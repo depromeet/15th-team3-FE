cb5e63ad8d29c032931bc8fa3962b19afd4708a9
semnil5202 <semnil5202@gmail.com>
2024-08-03 01:41:58 +0900

    refactor: fix prefetching error temporary

--- packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
+++ packages/web-domains/src/relay-question/screens/SelectRelayQuestionScreen.tsx
@@ -1,41 +1,41 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { Suspense } from 'react';
+// import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+// import { Suspense } from 'react';
 
 import { ContentContainer } from '../features/select-relay-question/containers/ContentContainer/ContentContainer';
 import { ProgressIndicatorContainer } from '../features/select-relay-question/containers/ProgressIndicatorContainer/ProgressIndicatorContainer';
 import { RandomPickContainer } from '../features/select-relay-question/containers/RandomPickContainer/RandomPickContainer';
 import { QueryStringProvider } from '../features/select-relay-question/contexts/QueryStringContext';
-import { useRelayQuestionListQueryPrefetch } from '../features/select-relay-question/hooks/queries/useRelayQuestionListQuery';
-import { MY_MEETINGS_QUERY_KEY } from '../features/start-relay-question/hooks/queries/useMyMeetingsQuery';
-import { MyMeetingsResponse } from '../features/start-relay-question/types';
+// import { useRelayQuestionListQueryPrefetch } from '../features/select-relay-question/hooks/queries/useRelayQuestionListQuery';
+// import { MY_MEETINGS_QUERY_KEY } from '../features/start-relay-question/hooks/queries/useMyMeetingsQuery';
+// import { MyMeetingsResponse } from '../features/start-relay-question/types';
 
 export const SelectRelayQuestionScreen = async () => {
-  const queryClient = await getServerSideProps();
+  // const queryClient = await getServerSideProps();
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense>
-        <QueryStringProvider>
-          <ProgressIndicatorContainer />
-          <ContentContainer />
-          <RandomPickContainer />
-        </QueryStringProvider>
-      </Suspense>
-    </HydrationBoundary>
+    // <HydrationBoundary state={dehydrate(queryClient)}>
+    // <Suspense>
+    <QueryStringProvider>
+      <ProgressIndicatorContainer />
+      <ContentContainer />
+      <RandomPickContainer />
+    </QueryStringProvider>
+    // </Suspense>
+    // </HydrationBoundary>
   );
 };
 
-const getServerSideProps = async () => {
-  const queryClient = new QueryClient();
+// const getServerSideProps = async () => {
+//   const queryClient = new QueryClient();
 
-  try {
-    const meetingId = queryClient.getQueryData<MyMeetingsResponse>([MY_MEETINGS_QUERY_KEY])?.meetingIds[0];
-    const relayQuestionListPrefetch = useRelayQuestionListQueryPrefetch({ queryClient, meetingId: meetingId || -1 });
+//   try {
+//     const meetingId = queryClient.getQueryData<MyMeetingsResponse>([MY_MEETINGS_QUERY_KEY])?.meetingIds[0];
+//     const relayQuestionListPrefetch = useRelayQuestionListQueryPrefetch({ queryClient, meetingId: meetingId || -1 });
 
-    await Promise.all([relayQuestionListPrefetch]);
-  } catch (error: unknown) {
-    console.log(error);
-  }
+//     await Promise.all([relayQuestionListPrefetch]);
+//   } catch (error: unknown) {
+//     console.log(error);
+//   }
 
-  return queryClient;
-};
+//   return queryClient;
+// };
