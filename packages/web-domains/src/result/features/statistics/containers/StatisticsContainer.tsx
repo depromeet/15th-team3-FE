d098c6eac8dc322c7a71dd018a5cdb4e0721fba1
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-04 05:03:46 +0900

    fix

--- packages/web-domains/src/result/features/statistics/containers/StatisticsContainer.tsx
+++ packages/web-domains/src/result/features/statistics/containers/StatisticsContainer.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { borderRadiusVariants, colors } from '@sambad/sds/theme';
-import { Fragment } from 'react';
 
 import { useGetMeetings } from '@/result/common/apis/queries/useGetMeetings';
+import { useGetStatistics } from '@/result/common/apis/queries/useGetStatistics';
 import { BaseParams } from '@/result/common/types/BaseParams';
 
 import { RankListHeader, RankListItem } from '../components';
@@ -12,46 +12,11 @@ export const StatisticsContainer = (params: BaseParams) => {
   const { data: meetingsIdsData } = useGetMeetings();
   // NOTE: 현재 스팩에서는 하나의 모임에만 가입할 수 있습니다.
   const meetingId = meetingsIdsData?.meetingIds[0] || -1;
-  //   const { data } = useGetStatistics({ meetingId, questionId: params.questionId });
+  const { data } = useGetStatistics({ meetingId, questionId: params.questionId });
 
   const style = {
     backgroundColor: colors.white,
     borderRadius: `0 0 ${borderRadiusVariants.medium} ${borderRadiusVariants.medium}`,
-  };
-
-  const data = {
-    contents: [
-      {
-        rank: 1,
-        answerContent: '💩 똥',
-        count: 14,
-        percentage: 50,
-      },
-      {
-        rank: 2,
-        answerContent: '💩 똥',
-        count: 14,
-        percentage: 50,
-      },
-      {
-        rank: 3,
-        answerContent: '💩 똥',
-        count: 14,
-        percentage: 50,
-      },
-      {
-        rank: 4,
-        answerContent: '💩 똥',
-        count: 14,
-        percentage: 50,
-      },
-      {
-        rank: 5,
-        answerContent: '💩 똥',
-        count: 14,
-        percentage: 50,
-      },
-    ],
   };
 
   return (
