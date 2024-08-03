ac1a1c95cf55dbab757339fe824e46f5cccfec18
leejeongho <jh9628771@gmail.com>
2024-08-03 20:49:40 +0900

    fix: api baseURL 수정

--- packages/web-domains/src/common/apis/base.api.ts
+++ packages/web-domains/src/common/apis/base.api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = `https://dev-api.moring.one/`;
+const baseURL = `https://dev-api.moring.one`;
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
