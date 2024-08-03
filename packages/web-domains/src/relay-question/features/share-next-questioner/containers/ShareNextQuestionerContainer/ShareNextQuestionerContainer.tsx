8ddba33b1debf7a1745b8f2fd51d42e7113123d3
semnil5202 <semnil5202@gmail.com>
2024-08-03 20:32:43 +0900

    feat: 공유하기 모달 구현

--- packages/web-domains/src/relay-question/features/share-next-questioner/containers/ShareNextQuestionerContainer/ShareNextQuestionerContainer.tsx
+++ packages/web-domains/src/relay-question/features/share-next-questioner/containers/ShareNextQuestionerContainer/ShareNextQuestionerContainer.tsx
@@ -5,6 +5,9 @@ import { colors, size } from '@sambad/sds/theme';
 
 import { ShareGroupShareIcon } from '../../../../assets/ShareGroupShareIcon';
 import { ShareNextQuestionerBackground } from '../../../../assets/ShareNextQuestionerBackground';
+import { Toast } from '../../../../common/Toast';
+import { useModal } from '../../../select-relay-question/hooks/useModal';
+import { ShareRelayQuestion } from '../../../share-group/containers/ShareRelayQuestion/ShareRelayQuestion';
 
 import {
   buttonWrapperCss,
@@ -15,8 +18,20 @@ import {
 } from './ShareNextQuestionerContainer.styles';
 
 export const ShareNextQuestionerContainer = () => {
+  const openModal = useModal();
+
+  const handleOpenShare = () => {
+    openModal({
+      component: ShareRelayQuestion,
+      componentProps: { title: '다음 릴레이 질문인에게' },
+    });
+  };
+
   return (
     <section css={wrapperCss}>
+      <Toast isOpen showTime={5000}>
+        asdf
+      </Toast>
       <div css={headingWrapperCss}>
         <Txt color={colors.black} typography="heading1" fontWeight="bold">
           다음 릴레이 질문을 만드는
@@ -32,7 +47,7 @@ export const ShareNextQuestionerContainer = () => {
       <div css={footerWrapperCss}>
         <ShareNextQuestionerBackground />
         <div css={buttonWrapperCss}>
-          <Button css={shareButtonCss}>
+          <Button css={shareButtonCss} onClick={handleOpenShare}>
             <ShareGroupShareIcon css={{ marginRight: size['6xs'] }} />
             다음 질문인에게 공유하기
           </Button>
