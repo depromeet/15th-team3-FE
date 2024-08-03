847b9df14c8586f46d3c390343973347b0dd84d5
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-03 23:53:29 +0900

    chore: add sadUserIcon

--- packages/core/sds/src/components/Icon/constants.ts
+++ packages/core/sds/src/components/Icon/constants.ts
@@ -5,6 +5,7 @@ import { AngleSmallDownIcon } from './assets/AngleSmallDown';
 import { AngleSmallUpIcon } from './assets/AngleSmallUp';
 import { AngleUpIcon } from './assets/AngleUp';
 import { CaretDownIcon } from './assets/CaretDown';
+import { SadUserIcon } from './assets/SadUserIcon';
 
 export const iconMap = {
   'angle-down': AngleDownIcon,
@@ -14,4 +15,5 @@ export const iconMap = {
   'angle-small-up': AngleSmallUpIcon,
   'angle-up': AngleUpIcon,
   'caret-down': CaretDownIcon,
+  'sad-user': SadUserIcon,
 };
