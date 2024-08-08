ce1ddf88a15457a85b54886c9be703628174f1e3
semnil5202 <semnil5202@gmail.com>
2024-08-08 23:09:49 +0900

    fix: undefined window 문제 해결

--- packages/web-domains/src/relay-question/utils/shareToKakao.ts
+++ packages/web-domains/src/relay-question/utils/shareToKakao.ts
@@ -14,29 +14,30 @@ interface ShareToKakaoProps {
 }
 
 const INIT_NAME = '000';
+const locationHref = typeof window === 'undefined' ? '' : window.location.href;
 
 const openGraphMap = {
   inviteGroup: {
     imageUrl: 'https://file.moring.one/defaults/invite_narrow.png',
     description: '삼봤드의 모험 모임에 여러분들을 초대합니다',
-    shareLink: window.location.href,
+    shareLink: locationHref,
   },
   newQuestion: {
     imageUrl: 'https://file.moring.one/defaults/new_question_narrow.png',
     description: (questionerName: string) =>
       `새로운 질문이 도착했어요! 지금 바로 답변 하러 가볼까요? 다음 질문인은 ${questionerName}님이에요`,
-    shareLink: (questionId: number) => `${window.location.href}?question-id=${questionId}`,
+    shareLink: (questionId: number) => `${locationHref}?question-id=${questionId}`,
   },
   nextQuestioner: {
     imageUrl: 'https://file.moring.one/defaults/new_question_narrow.png',
     description: (questionerName: string) =>
       `다음 질문인은 ${questionerName}님이에요! 현재 진행 중인 릴레이 질문이 끝나면 질문을 꼭! 생성해주세요`,
-    shareLink: window.location.href,
+    shareLink: locationHref,
   },
   hurryAnswer: {
     imageUrl: 'https://file.moring.one/defaults/question_narrow.png',
     description: '삼봤드의 모험 모임 질문을 잊진 않으셨나요?',
-    shareLink: window.location.href,
+    shareLink: locationHref,
   },
 };
 
