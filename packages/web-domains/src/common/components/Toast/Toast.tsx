32e2a134bba0c38bb1e83b0e5f1bfec3647b6dab
semnil5202 <semnil5202@gmail.com>
2024-08-08 21:15:49 +0900

    feat: Toast 컴포넌트 구현

---
+++ packages/web-domains/src/common/components/Toast/Toast.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Txt } from '@sambad/sds/components';
+import { colors, size } from '@sambad/sds/theme';
+import { HTMLAttributes, useEffect, useState } from 'react';
+
+import { wrapperCss } from './Toast.styles';
+
+import { ToastCheck } from '@/common/assets/ToastCheck';
+
+interface ToastProps extends HTMLAttributes<HTMLDivElement> {
+  isOpen: boolean;
+  showTime?: number;
+  children: string;
+}
+
+export const Toast = ({ children, showTime = 1500, isOpen, ...rest }: ToastProps) => {
+  const [isShow, setIsShow] = useState<boolean>(isOpen);
+  const safeShowTime = showTime < 1000 ? 1000 : showTime;
+
+  useEffect(() => {
+    if (!isShow) return;
+
+    const timer = setTimeout(() => {
+      setIsShow(false);
+    }, safeShowTime);
+
+    return () => clearTimeout(timer);
+  }, [isShow, safeShowTime]);
+
+  return (
+    <>
+      {isShow && (
+        <div css={wrapperCss} {...rest}>
+          <ToastCheck css={{ marginBottom: size['4xs'] }} />
+          <Txt color={colors.white} typography="title3" fontWeight="medium">
+            {children}
+          </Txt>
+        </div>
+      )}
+    </>
+  );
+};
