501691495dbed95fa400ae03e1b5f82c36f1a0f7
semnil5202 <semnil5202@gmail.com>
2024-08-10 00:46:52 +0900

    refactor: add comment in copyToClipboard

--- packages/web-domains/src/common/components/KakaoShare/copyToClipboard.utils.ts
+++ packages/web-domains/src/common/components/KakaoShare/copyToClipboard.utils.ts
@@ -1,7 +1,9 @@
 export const copyToClipboard = async (textToCopy: string) => {
+  // https 면 if 문 수행
   if (navigator.clipboard && window.isSecureContext) {
     await navigator.clipboard.writeText(textToCopy);
   } else {
+    // 화면에 보이지 않는 textarea를 body 앞(prepend) 생성
     const textArea = document.createElement('textarea');
     textArea.value = textToCopy;
 
