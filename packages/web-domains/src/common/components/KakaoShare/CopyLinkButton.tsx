538942b511f80f8a4c48148dc1868a8c96fec86a
semnil5202 <semnil5202@gmail.com>
2024-08-10 00:35:18 +0900

    refactor: edit prop and delete Txt default value attribute

--- packages/web-domains/src/common/components/KakaoShare/CopyLinkButton.tsx
+++ packages/web-domains/src/common/components/KakaoShare/CopyLinkButton.tsx
@@ -5,11 +5,11 @@ import { HTMLAttributes } from 'react';
 import { wrapperCss } from './CopyLinkButton.styles';
 import { CopyLinkIcon } from './CopyLinkIcon';
 
-export const CopyLinkButton = ({ ...rest }: HTMLAttributes<HTMLDivElement>) => {
+export const CopyLinkButton = (rest: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div css={wrapperCss} {...rest}>
       <CopyLinkIcon />
-      <Txt color={colors.grey700} typography="title4" fontWeight="medium">
+      <Txt color={colors.grey700} typography="title4">
         링크 복사
       </Txt>
     </div>
