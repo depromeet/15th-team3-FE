de9959aab2553596b233f09319acc0b6b589ae42
abg3000 <abg3000@naver.com>
2024-08-13 00:12:34 +0900

    feat: 질문 답변후 invalidate

--- packages/web-domains/src/answer/features/comment/services/useCommentService.tsx
+++ packages/web-domains/src/answer/features/comment/services/useCommentService.tsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { isAxiosError } from 'axios';
 import { useAtomValue } from 'jotai';
 import { useParams, useRouter } from 'next/navigation';
@@ -5,10 +6,14 @@ import { useState } from 'react';
 
 import { useAnswerQuestionMutation } from '@/answer/common/apis/mutations/useAnswerQuestionMutation';
 import { useCommentMutation } from '@/answer/common/apis/mutations/useCommentMutation';
+import { PROGRESSING_QUESTION_QUERY_KEY } from '@/answer/common/apis/queries/useGetProgressingQuestion';
 import { answerAtoms } from '@/answer/common/atoms/answer.atom';
+import { GATHER_MEMBER_QUERY_KEY } from '@/home/common/apis/queries/useGetGatherMemberList';
 import { useGetMeetingInfo } from '@/home/common/apis/queries/useGetMeetingName';
+import { TOP_PREVIOUS_QUESTION_QUERY_KEY } from '@/home/common/apis/queries/useGetTopPreviousQuestionList';
 
 export const useCommentService = () => {
+  const queryClient = useQueryClient();
   const { push } = useRouter();
   const [comment, setComment] = useState<string>('');
   const answerList = useAtomValue(answerAtoms.answerList);
@@ -28,6 +33,10 @@ export const useCommentService = () => {
       if (comment.length) {
         await sendCommentMutate({ content: comment, meetingId, meetingQuestionId: questionId });
       }
+
+      queryClient.invalidateQueries({
+        queryKey: [PROGRESSING_QUESTION_QUERY_KEY, TOP_PREVIOUS_QUESTION_QUERY_KEY, GATHER_MEMBER_QUERY_KEY],
+      });
       push('/answer/closing');
     } catch (error) {
       if (isAxiosError(error)) {
