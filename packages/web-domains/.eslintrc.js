a58d10cbb10452f01a0dfcfcf0ad8feae74f9cfa
이세민 <89172499+semnil5202@users.noreply.github.com>
2024-08-14 22:51:20 +0900

    refactor(web-domains): 카카오톡 공유하기 모달 interface 수정 (#99)
    * chore: eslint extends react-internal -> next.js로 수정
    
    * refactor: KakaoShareModal Interface 수정 및 qs 오류 수정

--- packages/web-domains/.eslintrc.js
+++ packages/web-domains/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@sambad/eslint-config/react-internal.js'],
+  extends: ['@sambad/eslint-config/next.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
--- packages/web-domains/src/common/components/KakaoShare/KakaoShare.tsx
+++ packages/web-domains/src/common/components/KakaoShare/KakaoShare.tsx
@@ -1,20 +1,20 @@
 import { Txt } from '@sambad/sds/components';
 import { colors } from '@sambad/sds/theme';
 
-import { KakaoShareOpenGraphKeyType, generateKakaoShare } from './generateKakaoShare.utils';
+import { generateKakaoShare } from './generateKakaoShare.utils';
 import { wrapperCss } from './KakaoShare.styles';
 import { KakaoShareIcon } from './KakaoShareIcon';
 
 interface ShareKakaoProps {
-  openGraphKey: KakaoShareOpenGraphKeyType;
-  questionId: number;
-  questionerName: string;
+  shareImageUrl: string;
+  shareDescription: string;
+  shareLink: string;
 }
 
-export const ShareKakaoButton = ({ openGraphKey, questionId, questionerName }: ShareKakaoProps) => {
+export const ShareKakaoButton = ({ shareImageUrl, shareDescription, shareLink }: ShareKakaoProps) => {
   return (
     <div css={wrapperCss}>
-      <KakaoShareIcon onClick={() => generateKakaoShare({ openGraphKey, questionId, questionerName })} />
+      <KakaoShareIcon onClick={() => generateKakaoShare({ shareImageUrl, shareDescription, shareLink })} />
       <Txt color={colors.grey700} typography="title4" fontWeight="medium">
         카카오톡 공유
       </Txt>
