8e0714d60e69bb9b4ba1727dce3dbaf4ebb7570c
semnil5202 <semnil5202@gmail.com>
2024-08-15 01:12:38 +0900

    refactor: 카카오톡 공유하기 모달 common 것으로 적용

--- packages/web-domains/src/relay-question/features/share-next-questioner/containers/ShareNextQuestionerContainer/ShareNextQuestionerContainer.tsx
+++ packages/web-domains/src/relay-question/features/share-next-questioner/containers/ShareNextQuestionerContainer/ShareNextQuestionerContainer.tsx
@@ -3,12 +3,16 @@
 import { Button, Txt } from '@sambad/sds/components';
 import { colors, size } from '@sambad/sds/theme';
 import Link from 'next/link';
+import { useState } from 'react';
+
+import { KakaoShareModal } from '@/common';
+import { getWebDomain } from '@/common/getWebdomain';
+import { useActiveQuestionQuery } from '@/relay-question/features/share-group/hooks/useActiveQuestionQuery';
+import { useMyMeetingsQuery } from '@/relay-question/features/start-relay-question/hooks/queries/useMyMeetingsQuery';
+import { findCurrentMeetingId } from '@/relay-question/utils/findCurrentMeetingId';
 
 import { ShareGroupShareIcon } from '../../../../assets/ShareGroupShareIcon';
 import { ShareNextQuestionerBackground } from '../../../../assets/ShareNextQuestionerBackground';
-import { useModal } from '../../../../hooks/useModal';
-import { KakaoShareOpenGraphKeyType } from '../../../../utils/shareToKakao';
-import { ShareRelayQuestion } from '../../../share-group/containers/ShareRelayQuestion/ShareRelayQuestion';
 
 import {
   buttonWrapperCss,
@@ -18,42 +22,56 @@ import {
   wrapperCss,
 } from './ShareNextQuestionerContainer.styles';
 
+const KAKAO_IMAGE_URL = 'https://file.moring.one/defaults/new_question_narrow.png';
+
 export const ShareNextQuestionerContainer = () => {
-  const openModal = useModal();
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
+  const { myMeetings } = useMyMeetingsQuery();
+  const { activeQuestion } = useActiveQuestionQuery(findCurrentMeetingId(myMeetings));
 
   const handleOpenShare = () => {
-    openModal({
-      component: ShareRelayQuestion,
-      componentProps: { title: '다음 릴레이 질문인에게', openGraphKey: 'nextQuestioner' as KakaoShareOpenGraphKeyType },
-    });
+    setIsOpenModal(true);
   };
 
   return (
-    <section css={wrapperCss}>
-      <div css={headingWrapperCss}>
-        <Txt color={colors.black} typography="heading1" fontWeight="bold">
-          다음 릴레이 질문을 만드는
-        </Txt>
-        <Txt color={colors.black} typography="heading1" fontWeight="bold">
-          <Txt color={colors.primary500} typography="heading1" fontWeight="bold">
-            질문인
+    <>
+      <section css={wrapperCss}>
+        <div css={headingWrapperCss}>
+          <Txt color={colors.black} typography="heading1" fontWeight="bold">
+            다음 릴레이 질문을 만드는
           </Txt>
-          에게 알려보세요!
-        </Txt>
-      </div>
-
-      <div css={footerWrapperCss}>
-        <ShareNextQuestionerBackground />
-        <div css={buttonWrapperCss}>
-          <Button css={shareButtonCss} onClick={handleOpenShare}>
-            <ShareGroupShareIcon css={{ marginRight: size['6xs'] }} />
-            다음 질문인에게 공유하기
-          </Button>
-          <Link href="/answer/opening">
-            <Button variant="sub">제일 먼저 답변하기</Button>
-          </Link>
+          <Txt color={colors.black} typography="heading1" fontWeight="bold">
+            <Txt color={colors.primary500} typography="heading1" fontWeight="bold">
+              질문인
+            </Txt>
+            에게 알려보세요!
+          </Txt>
         </div>
-      </div>
-    </section>
+
+        <div css={footerWrapperCss}>
+          <ShareNextQuestionerBackground />
+          <div css={buttonWrapperCss}>
+            <Button css={shareButtonCss} onClick={handleOpenShare}>
+              <ShareGroupShareIcon css={{ marginRight: size['6xs'] }} />
+              다음 질문인에게 공유하기
+            </Button>
+            <Link href="/answer/opening">
+              <Button variant="sub">제일 먼저 답변하기</Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <KakaoShareModal
+        isOpen={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+        topTitle="다음 릴레이 질문인에게"
+        bottomTitle="공유해 보세요!"
+        shareImageUrl={KAKAO_IMAGE_URL}
+        shareDescription={`다음 질문인은 ${activeQuestion?.targetMember.name}님이에요! 현재 진행 중인 릴레이 질문이 끝나면 질문을 꼭! 생성해주세요`}
+        shareLink={`${getWebDomain()}/answer/opening`}
+      />
+    </>
   );
 };
