cc386a44388c01549c3eb173388780b1bdb32c03
abg3000 <abg3000@naver.com>
2024-08-18 00:06:41 +0900

    chore: test

--- packages/web-domains/src/common/utils/getWebDomain.ts
+++ packages/web-domains/src/common/utils/getWebDomain.ts
@@ -1,6 +1,9 @@
 const STAGE = process.env.NEXT_PUBLIC_STAGE;
 
 export const getWebDomain = () => {
+  /**
+   * 왜 안됨?
+   */
   switch (STAGE) {
     case 'production':
       return 'https://moring.one';
