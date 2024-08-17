98c24fac5fde2e4d67a316c03fc4d67ecaae6da7
abg3000 <abg3000@naver.com>
2024-08-17 23:39:35 +0900

    fix: 환경변수 설정 위치 변경

--- packages/web-domains/src/common/utils/getWebDomain.ts
+++ packages/web-domains/src/common/utils/getWebDomain.ts
@@ -1,6 +1,5 @@
+const STAGE = process.env.NEXT_PUBLIC_STAGE;
 export const getWebDomain = () => {
-  const STAGE = process.env.NEXT_PUBLIC_STAGE;
-
   switch (STAGE) {
     case 'production':
       return 'https://moring.one';
