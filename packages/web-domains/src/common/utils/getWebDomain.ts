6b129442c5ca0d709fffc4ebf090d489db8b4d4b
abg3000 <abg3000@naver.com>
2024-08-18 10:13:32 +0900

    chore:test

--- packages/web-domains/src/common/utils/getWebDomain.ts
+++ packages/web-domains/src/common/utils/getWebDomain.ts
@@ -1,12 +1,11 @@
 const STAGE = process.env.NEXT_PUBLIC_STAGE;
 
 export const getWebDomain = () => {
-  if (STAGE === 'production') {
-    return 'https://moring.one';
-  }
-
-  if (STAGE === 'development') {
-    return 'https://dev.moring.one';
+  switch (STAGE) {
+    case 'production':
+      return 'https://moring.one';
+    case 'development':
+      return 'https://dev.moring.one';
   }
 
   return 'https://local.moring.one:3000';
