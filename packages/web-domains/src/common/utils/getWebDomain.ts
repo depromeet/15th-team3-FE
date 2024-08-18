718b0859a8efa8ff1bd21c8523a41f406bb85a7d
abg3000 <abg3000@naver.com>
2024-08-18 10:01:41 +0900

    chore: remove console

--- packages/web-domains/src/common/utils/getWebDomain.ts
+++ packages/web-domains/src/common/utils/getWebDomain.ts
@@ -1,16 +1,11 @@
 const STAGE = process.env.NEXT_PUBLIC_STAGE;
 
 export const getWebDomain = () => {
-  console.log(process.env.NEXT_PUBLIC_STAGE);
-  console.log({ STAGE });
-
   if (STAGE === 'production') {
-    console.log('prod');
     return 'https://moring.one';
   }
 
   if (STAGE === 'development') {
-    console.log('dev');
     return 'https://dev.moring.one';
   }
 
