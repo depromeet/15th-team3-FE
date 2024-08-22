33118f2c1eab9b638c7f0a584e312d4e5a48f6ac
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-22 22:45:04 +0900

    feat: modify 페이지인지 아닌지에 따라 버튼 텍스트 및 이벤트 핸들러 분리 처리

--- packages/web-domains/src/about-me/features/containers/ScreenContainer.tsx
+++ packages/web-domains/src/about-me/features/containers/ScreenContainer.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { colors, shadow, size } from '@sambad/sds/theme';
-import { Button } from '@sds/components';
+import { Button, TextButton, Txt } from '@sds/components';
+import { useRouter } from 'next/navigation';
 
 import { useCreateHandWavings } from '@/about-me/common/apis/mutates/useCreateHandWavings';
 import { useGetHandWavingsStatus } from '@/about-me/common/apis/queries/useGetHandWavingsStatus';
 import { ActionBar } from '@/common/components/ActionBar/ActionBar';
 
 import { useGetFirstMeetingId } from '../hooks/useGetFirstMeetingId';
+import { useGetIsModifyPage } from '../hooks/useGetIsModifyPage';
 import { useIsMyByParams } from '../hooks/useIsMyByParams';
 
 import { ProfileContainer } from './ProfileContainer';
@@ -15,6 +17,9 @@ import { SegmentedControlContainer } from './SegmentedControlContainer';
 import { handWavingButtonCss, screenRootCss } from './styles';
 
 export const ScreenContainer = () => {
+  const isModifyPage = useGetIsModifyPage();
+  const router = useRouter();
+
   const { isMy, meetingMemberId } = useIsMyByParams();
   const { meetingId } = useGetFirstMeetingId();
   const { data: wavingStatusData, isSuccess: getWavingStatusSuccess } = useGetHandWavingsStatus({
@@ -29,9 +34,33 @@ export const ScreenContainer = () => {
     mutate({ meetingId, receiverMemberId: meetingMemberId });
   };
 
+  const handleMoveToModifyPage = () => {
+    router.push('/about/me/modify');
+  };
+
+  const handleModify = () => {
+    // 수정 api
+  };
+
   return (
     <div css={screenRootCss}>
-      <ActionBar title={isMy ? '마이 프로필' : '프로필'} />
+      <ActionBar
+        title={isMy ? '마이 프로필' : '프로필'}
+        rightDecor={
+          isMy && (
+            <TextButton
+              variant="normal"
+              color={colors.primary500}
+              onClick={isModifyPage ? handleModify : handleMoveToModifyPage}
+            >
+              {/* NOTE: sds에서 Txt 컴포넌트가 css로 오버라이딩이 되지 않는 무제 해결 후 inherit 제거 예정 */}
+              <Txt typography="subTitle2" style={{ color: 'inherit' }}>
+                {isModifyPage ? '수정완료' : '수정하기'}
+              </Txt>
+            </TextButton>
+          )
+        }
+      />
       <div style={layoutStyle}>
         <ProfileContainer style={{ marginBottom: size['5xs'] }} />
         <SegmentedControlContainer style={sectionStyle} />
---
+++ packages/web-domains/src/about-me/screens/AboutMeModifyScreen.tsx
@@ -0,0 +1,54 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+
+import { getAnswersPrefetch } from '../common/apis/queries/useGetAnswers';
+import { getAnswersMePrefetch } from '../common/apis/queries/useGetAnswersMe';
+import { getMeetingsPrefetch, MEETINGS_QUERY_KEY } from '../common/apis/queries/useGetMeetings';
+import { getMemberPrefetch } from '../common/apis/queries/useGetMember';
+import { getMemberMePrefetch } from '../common/apis/queries/useGetMemberMe';
+import { MeetingResponse } from '../common/apis/schema/MeetingResponse';
+import { ScreenContainer } from '../features/containers/ScreenContainer';
+
+interface AboutMeScreenParams {
+  meetingMemberId?: number;
+}
+
+export const AboutMeModifyScreen = async (params: AboutMeScreenParams) => {
+  const { meetingMemberId } = params;
+  const { queryClient } = await getServerSideProps(meetingMemberId);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ScreenContainer />
+    </HydrationBoundary>
+  );
+};
+
+export const getServerSideProps = async (meetingMemberId?: number) => {
+  const queryClient = new QueryClient();
+
+  try {
+    await getMeetingsPrefetch({ queryClient });
+    const data = queryClient.getQueryData<MeetingResponse>([MEETINGS_QUERY_KEY]);
+    const meetingId = data?.meetings[0]?.meetingId;
+
+    if (!meetingId) {
+      throw new Error('No meetingId found');
+    }
+
+    if (meetingMemberId) {
+      await Promise.all([
+        getMemberPrefetch({ meetingId, meetingMemberId, queryClient }),
+        getAnswersPrefetch({ meetingId, meetingMemberId, queryClient }),
+      ]);
+    } else {
+      await Promise.all([
+        getMemberMePrefetch({ meetingId, queryClient }),
+        getAnswersMePrefetch({ meetingId, queryClient }),
+      ]);
+    }
+  } catch (error: unknown) {
+    console.error(error);
+  }
+
+  return { queryClient };
+};
