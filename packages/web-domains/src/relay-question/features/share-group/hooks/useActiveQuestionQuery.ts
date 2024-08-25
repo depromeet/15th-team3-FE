ae4010c805705d7d5986725b05b1f339360cf65a
이세민 <89172499+semnil5202@users.noreply.github.com>
2024-08-25 14:18:07 +0900

    refactor(*): 릴레이 질문 시작 지면 [meetingId] Dynamic Routing 대응 (#131)
    * refactor: start-relay-question meetingId params 대응
    
    * refactor: select-question meetingId params 대응 및 object-fit cover
    
    * refactor: Share Group 페이지 meetingId params 대응
    
    * fix: share group prefetch myInfo -> activeQuestion
    
    * refactor: share next questioner meetingId params 대응
    
    * fix: 누락된 meetingId 대응

--- packages/web-domains/src/relay-question/features/share-group/hooks/useActiveQuestionQuery.ts
+++ packages/web-domains/src/relay-question/features/share-group/hooks/useActiveQuestionQuery.ts
@@ -1,12 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
+import { QueryClient, useQuery } from '@tanstack/react-query';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 
 import { Http } from '../../../../common/apis/base.api';
 import { ActiveQuestionResponse } from '../types';
 
 export const ACTIVE_QUESTION_QUERY_KEY = 'ACTIVE_QUESTION_QUERY_KEY';
 
-const _getActiveQuestion = async (meetingId: number) =>
-  await Http.GET<ActiveQuestionResponse>(`/v1/meetings/${meetingId}/questions/active`);
+const _getActiveQuestion = async (meetingId: number, cookie?: ReadonlyRequestCookies) =>
+  await Http.GET<ActiveQuestionResponse>(`/v1/meetings/${meetingId}/questions/active`, {
+    headers: {
+      Cookie: cookie?.toString(),
+    },
+  });
 
 export const useActiveQuestionQuery = (meetingId: number) => {
   const { data, ...rest } = useQuery({
@@ -16,4 +21,17 @@ export const useActiveQuestionQuery = (meetingId: number) => {
   });
 
   return { activeQuestion: data, ...rest };
+};
+
+export const getActiveQuestionPrefetch = (
+  meetingId: number,
+  queryClient: QueryClient,
+  cookie: ReadonlyRequestCookies,
+) => {
+  const prefetch = queryClient.prefetchQuery({
+    queryKey: [ACTIVE_QUESTION_QUERY_KEY, meetingId],
+    queryFn: () => _getActiveQuestion(meetingId, cookie),
+  });
+
+  return prefetch;
 };
