6a1753c19b5511545dce6ce28ccf1ca17ef76afe
leejeongho <92032081+LeeJeongHooo@users.noreply.github.com>
2024-08-25 14:10:56 +0900

    feat(web-domains): 유저 정보 수정 (#135)
    * fix: 모임원, 모임장 기본 정보 타이틀 수정
    
    * feat: 자기소개 기본정보 수정 UI 및 기능 추가
    
    * fix: 라우트 경로 설정 변경

---
+++ packages/web-domains/src/user/features/modify-user-info/components/ModifyIntroForm.tsx
@@ -0,0 +1,41 @@
+import { Txt, Button } from '@sds/components';
+import { colors } from '@sds/theme';
+import { useForm } from 'react-hook-form';
+
+import { buttonWrapperCss } from '../../get-user-info/components/Form/styles';
+import { TextArea } from '../../get-user-info/components/TextInput/TextArea';
+import { useModifyUserInfoService } from '../services/useModifyUserInfoService';
+
+export interface IntroFormType {
+  introduction: string;
+}
+
+const MAX_LENGTH = 3000;
+
+export const ModifyIntroForm = () => {
+  const {
+    register,
+    formState: { isValid },
+    handleSubmit,
+  } = useForm<IntroFormType>({ defaultValues: { introduction: '' } });
+
+  const { handleModifyUserInfo } = useModifyUserInfoService();
+
+  return (
+    <form onSubmit={handleSubmit(handleModifyUserInfo)} css={{ padding: '0 20px', marginTop: '48px' }}>
+      <TextArea
+        placeholder="저는 이런 사람이에요."
+        maxLength={MAX_LENGTH}
+        {...register('introduction', { maxLength: MAX_LENGTH })}
+      />
+      <Txt as="p" typography="body4" color={colors.grey600}>
+        최대 {MAX_LENGTH}자까지 입력해주세요
+      </Txt>
+      <div css={buttonWrapperCss}>
+        <Button type="submit" size="large" disabled={!isValid}>
+          완료
+        </Button>
+      </div>
+    </form>
+  );
+};
