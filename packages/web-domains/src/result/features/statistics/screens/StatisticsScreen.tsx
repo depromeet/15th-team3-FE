7c2adc71e121236d6b4dfd5ba45366dcfda08ade
Doeunnkimm <doeunnkimm@gmail.com>
2024-08-25 14:12:16 +0900

    feat: statistics 스크린 server side fetching

--- packages/web-domains/src/result/features/statistics/screens/StatisticsScreen.tsx
+++ packages/web-domains/src/result/features/statistics/screens/StatisticsScreen.tsx
@@ -1,13 +1,32 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+
+import { getStatisticsPrefetch } from '@/result/common/apis/queries/useGetStatistics';
 import { BaseLayout } from '@/result/common/components';
 import { BaseParams } from '@/result/common/types/BaseParams';
 
 import { HeaderContainer } from '../../main/containers';
 import { StatisticsContainer } from '../containers/StatisticsContainer';
 
-export const StatisticsScreen = (params: BaseParams) => {
+export const StatisticsScreen = async (params: BaseParams) => {
+  const { queryClient } = await getServerSideProps(params);
+
   return (
-    <BaseLayout title="전체 통계 결과" header={<HeaderContainer />}>
-      <StatisticsContainer />
-    </BaseLayout>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <BaseLayout title="전체 통계 결과" header={<HeaderContainer />}>
+        <StatisticsContainer />
+      </BaseLayout>
+    </HydrationBoundary>
   );
+};
+
+const getServerSideProps = async (params: BaseParams) => {
+  const queryClient = new QueryClient();
+
+  try {
+    getStatisticsPrefetch({ queryClient, meetingId: Number(params.meetingId), questionId: Number(params.questionId) });
+  } catch (error) {
+    console.error(error);
+  }
+
+  return { queryClient };
 };
