a4593db0c9bb20bccbe688c856441a901f05e16b
abg3000 <abg3000@naver.com>
2024-08-27 02:00:46 +0900

    fix: 아무도 답하지 않았을 때 결과페이지 대응

--- packages/web-domains/src/result/features/main/containers/MostAnsweredContainer.tsx
+++ packages/web-domains/src/result/features/main/containers/MostAnsweredContainer.tsx
@@ -12,18 +12,20 @@ export const MostAnsweredContainers = () => {
   const { data } = useGetMostSelected({ meetingId, questionId });
 
   // NOTE: 홍길동님 외 N명 <- 에서 홍길동 자리에 위치시킬 이름
-  const showName = data?.selectedMembers[0]?.name;
-  const profileUrls = data?.selectedMembers.map((member) => member.profileImageFileUrl);
+  const showName = data?.selectedMembers?.[0]?.name;
+  const profileUrls = data?.selectedMembers?.map((member) => member.profileImageFileUrl);
 
   return (
     <Section title="모임원들이 가장 많이 한 답변은?">
       <MostAnswered contents={data?.content} />
-      <CountByMemberList
-        showName={showName}
-        count={data?.count}
-        profileUrls={profileUrls}
-        href={`/${meetingId}/question-result/${questionId}/answers/most-selected`}
-      />
+      {data && showName && (
+        <CountByMemberList
+          showName={showName}
+          count={data?.count}
+          profileUrls={profileUrls}
+          href={`/${meetingId}/question-result/${questionId}/answers/most-selected`}
+        />
+      )}
     </Section>
   );
 };
