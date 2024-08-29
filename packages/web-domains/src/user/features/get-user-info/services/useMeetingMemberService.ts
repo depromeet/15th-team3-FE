1d3683163cab0ef8bcb07d93d38aa42fe0384ca5
Jong <abg3000@naver.com>
2024-08-29 09:44:36 +0900

    fix: 중복생성 방지 (#174)

--- packages/web-domains/src/user/features/get-user-info/services/useMeetingMemberService.ts
+++ packages/web-domains/src/user/features/get-user-info/services/useMeetingMemberService.ts
@@ -7,30 +7,32 @@ import { MEETING_INFO_QUERY_KEY } from '@/home/common/apis/queries/useGetMeeting
 export const useMeetingMemberService = () => {
   const router = useRouter();
 
-  const { mutateAsync: createMeetingMember } = useCreateMeetingMember();
+  const { mutateAsync: createMeetingMember, isPending } = useCreateMeetingMember();
   const queryClient = useQueryClient();
 
   const participateMeeting = async (params: Params) => {
-    await createMeetingMember(params, {
-      onSuccess: () => {
-        if (params.role === 'MEMBER') {
-          router.push(`/user/member/closing?inviteCode=${params.inviteCode}`);
-        }
-        // 오너로 가입된 경우
-        if (params.role === 'OWNER') {
-          queryClient.invalidateQueries({ queryKey: [MEETING_INFO_QUERY_KEY] });
-          router.push(`/meeting/new/closing?inviteCode=${params.inviteCode}`);
-        }
-      },
-      onError: (res) => {
-        if (res.response?.status === 404) {
-          alert('모임을 찾을 수 없습니다.');
-        }
-        if (res.response?.status === 409) {
-          alert('이미 가입된 모임입니다.');
-        }
-      },
-    });
+    if (!isPending) {
+      await createMeetingMember(params, {
+        onSuccess: () => {
+          if (params.role === 'MEMBER') {
+            router.push(`/user/member/closing?inviteCode=${params.inviteCode}`);
+          }
+          // 오너로 가입된 경우
+          if (params.role === 'OWNER') {
+            queryClient.invalidateQueries({ queryKey: [MEETING_INFO_QUERY_KEY] });
+            router.push(`/meeting/new/closing?inviteCode=${params.inviteCode}`);
+          }
+        },
+        onError: (res) => {
+          if (res.response?.status === 404) {
+            alert('모임을 찾을 수 없습니다.');
+          }
+          if (res.response?.status === 409) {
+            alert('이미 가입된 모임입니다.');
+          }
+        },
+      });
+    }
   };
 
   return { participateMeeting };
