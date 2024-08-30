9503dda22cd957495b7053555df25d16d097e384
semnil5202 <semnil5202@gmail.com>
2024-08-31 02:23:23 +0900

    refactor: skeleton width 수정

--- packages/web-domains/src/relay-question/features/select-relay-question/components/Skeleton/QuestionListSkeleton.tsx
+++ packages/web-domains/src/relay-question/features/select-relay-question/components/Skeleton/QuestionListSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@sds/components';
 export const QuestionListSkeleton = () => {
   return (
     <section style={{ paddingLeft: 20 }}>
-      <Skeleton width={280} height={36} style={{ marginTop: 40, marginBottom: 44 }} />
+      <Skeleton width={260} height={36} style={{ marginTop: 40, marginBottom: 44 }} />
       <div>
         {Array.from({ length: 8 }, (_, index) => (
           <Skeleton key={index} width={340} height={64} style={{ margin: '22px 0' }} />
