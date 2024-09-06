01d261b7aeca89694cc2deda57de0038d12857d3
leejeongho <jh9628771@gmail.com>
2024-09-06 23:06:39 +0900

    refact: SDS 템플릿 생성 자동화 도구 구축

--- packages/core/sds/turbo/generators/config.ts
+++ packages/core/sds/turbo/generators/config.ts
@@ -32,23 +32,50 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       },
     ],
     actions: (answers) => {
-      if (!answers) {
-        return [];
-      }
+      if (!answers) return [];
 
-      return [
-        {
-          type: 'add',
-          path: `src/{{ type }}/{{ name }}.${answers.type === 'components' ? 'tsx' : 'ts'}`,
-          templateFile: `templates/${answers.type === 'components' ? 'component' : 'theme'}.hbs`,
-        },
-        {
-          type: 'append',
-          path: 'src/{{ type }}/index.ts',
-          pattern: /(export {[^]*} from '[^]*';)/g,
-          template: "export { {{ name }} } from './{{ name }}'",
-        },
-      ];
+      switch (answers.type) {
+        case 'components':
+          return componentsActions;
+        case 'theme':
+          return themeActions;
+        default:
+          return [];
+      }
     },
   });
 }
+
+const componentsActions = [
+  {
+    type: 'add',
+    path: `src/{{ type }}/{{ name }}/{{ name }}.tsx`,
+    templateFile: `templates/component.hbs`,
+  },
+  {
+    type: 'add',
+    path: `src/{{ type }}/{{ name }}/index.ts`,
+    template: "export { {{ pascalCase name }} } from './{{ name }}'",
+  },
+  { type: 'add', path: `src/{{ type }}/{{ name }}/styles.ts` },
+  {
+    type: 'modify',
+    path: 'src/{{ type }}/index.ts',
+    pattern: /$(?![\s\S])/g,
+    template: "$&export * from './{{ name }}';\n",
+  },
+];
+
+const themeActions = [
+  {
+    type: 'add',
+    path: `src/{{ type }}/{{ name }}.ts`,
+    templateFile: `templates/theme.hbs`,
+  },
+  {
+    type: 'modify',
+    path: 'src/{{ type }}/index.ts',
+    pattern: /$(?![\s\S])/g,
+    template: "export { {{ name }} } from './{{ name }}';\n",
+  },
+];
