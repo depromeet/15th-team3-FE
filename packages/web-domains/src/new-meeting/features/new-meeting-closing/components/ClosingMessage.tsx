b4c9ff4f65fca4ae69b1f20bc078736590c5dfec
Doeunnkimm <doeunnkimm@gmail.com>
2024-09-13 18:58:56 +0900

    feat: 모임 추가 완료 시 confetti

--- packages/web-domains/src/new-meeting/features/new-meeting-closing/components/ClosingMessage.tsx
+++ packages/web-domains/src/new-meeting/features/new-meeting-closing/components/ClosingMessage.tsx
@@ -2,6 +2,8 @@ import { Txt } from '@sambad/sds/components';
 import { colors, size } from '@sambad/sds/theme';
 import Image from 'next/image';
 
+import { Confetti } from '@/common';
+
 import ClosingBackground from '../../../common/assets/images/closing-background.png';
 
 interface ClosingMessageProps {
@@ -16,44 +18,47 @@ const ClosingMessage = (props: ClosingMessageProps) => {
   }
 
   return (
-    <div
-      css={{
-        position: 'absolute',
-        top: '108px',
-        width: '100%',
-        zIndex: '1',
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <div css={{ transform: 'translate(0, 20%)' }}>
-        {name && (
-          <Txt as="h1" color={colors.primary500} typography="heading1">
-            {name}
-          </Txt>
-        )}
-        <Txt as="h2" color={colors.black} typography="heading1">
-          모임을 만들었어요!
-        </Txt>
-        <Txt as="p" color={colors.grey600} typography="body3" css={{ marginTop: size['6xs'] }}>
-          이제 모임원을 초대해보세요!
-        </Txt>
-      </div>
-
+    <>
+      <Confetti position={{ top: 0, left: 0 }} size={600} />
       <div
         css={{
           position: 'absolute',
-          width: '80%',
-          aspectRatio: '3/2',
-          maxWidth: '500px',
+          top: '108px',
+          width: '100%',
+          zIndex: '1',
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <Image alt="background-image" src={ClosingBackground} quality={100} fill style={{ objectFit: 'contain' }} />
+        <div css={{ transform: 'translate(0, 20%)' }}>
+          {name && (
+            <Txt as="h1" color={colors.primary500} typography="heading1">
+              {name}
+            </Txt>
+          )}
+          <Txt as="h2" color={colors.black} typography="heading1">
+            모임을 만들었어요!
+          </Txt>
+          <Txt as="p" color={colors.grey600} typography="body3" css={{ marginTop: size['6xs'] }}>
+            이제 모임원을 초대해보세요!
+          </Txt>
+        </div>
+
+        <div
+          css={{
+            position: 'absolute',
+            width: '80%',
+            aspectRatio: '3/2',
+            maxWidth: '500px',
+          }}
+        >
+          <Image alt="background-image" src={ClosingBackground} quality={100} fill style={{ objectFit: 'contain' }} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
